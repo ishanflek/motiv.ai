237f17dd3709a29bd57b8378bca50b150908b4f5
Mr.Dey <80757858+td-heisenbug@users.noreply.github.com>
2023-04-17 11:44:36 +0530

    Added codes for handling quote, needs minor fixes

--- screens/Editor.js
+++ screens/Editor.js
@@ -13,7 +13,8 @@ import {
   Alert,
   Platform,
   Dimensions,
-  Share
+  Share,
+  ActivityIndicator
 } from 'react-native';
 
 import Slider from '@react-native-community/slider';
@@ -24,7 +25,7 @@ import auth from '@react-native-firebase/auth';
 import { white } from 'react-native-paper/lib/typescript/src/styles/themes/v2/colors';
 
 function Editor({route, navigation}) {
-  const [text, setText] = useState('');
+  const [text, setText] = useState(route.params.quote);
   const [editing, setEditing] = useState(true);
   const imgurl = route.params.link
   const fs = Dimensions.get("window").fontScale;
@@ -32,13 +33,50 @@ function Editor({route, navigation}) {
   const [sliderValue, setSliderValue] = useState(12);
   const viewRef = useRef();
   const [txtcolor, setTxtColor] = useState('#000000');
+  const [loading, setLoading] = useState(false);
+  const [hue, setHue] = useState(0);
+
+
   let isImageUploaded = false;
   let downloadUrl = ""
 
+  function hsvToRgb(h, s, v) {
+    const c = v * s;
+    const hp = h / 60;
+    const x = c * (1 - Math.abs((hp % 2) - 1));
+    const rgb1 = [c, x, 0];
+    const rgb2 = [x, c, 0];
+    const rgb3 = [0, c, x];
+    const rgb4 = [0, x, c];
+    const rgb5 = [x, 0, c];
+    const rgb6 = [c, 0, x];
+    const segments = [
+      [rgb1, rgb6],
+      [rgb2, rgb1],
+      [rgb3, rgb1],
+      [rgb1, rgb4],
+      [rgb5, rgb1],
+      [rgb1, rgb3],
+    ];
+    const segmentIndex = Math.floor(hp) % 6;
+    const t = (hp % 1) * 2 - 1;
+    const color1 = segments[segmentIndex][0];
+    const color2 = segments[segmentIndex][1];
+    return [
+      (color1[0] + (color2[0] - color1[0]) * t) * 255,
+      (color1[1] + (color2[1] - color1[1]) * t) * 255,
+      (color1[2] + (color2[2] - color1[2]) * t) * 255,
+    ];
+  }
+  
 
-  const handleColorChange = (newColor) => {
-    setColor(newColor);
+
+  const handleColorChange = (hue) => {
+    setHue(hue);
+    const [r, g, b] = hsvToRgb(hue, 65, 80);
+    setTxtColor(`rgb(${r}, ${g}, ${b})`);
   };
+  
 
 
 
@@ -82,47 +120,44 @@ function Editor({route, navigation}) {
     }
 };
 
-  const downloadImage = async () => {
-    try {
-      const uri = await captureRef(viewRef, {
-        format: 'png',
-        quality: 0.8,
-      });
-      
-      if (Platform.OS === 'android') {
-        const granted = await getPermissionAndroid();
-        if (!granted) {
-          return;
-        }
-      }
-      console.log(uri);
-      const currentUser = auth().currentUser;
-      const uid = currentUser.uid;
-      imgName= '1322211.png';
-      let reference = storage().ref().child(uid).child(imgName);     
-      let task = reference.putFile(uri);              
-      Alert.alert('Uploading the image, wait till uploading finishes')
-      task.then(() => {
-        console.log('Image uploaded to the bucket!');
-        reference.getDownloadURL().then((url) => {
-            console.log('Download URL:', url);
-            downloadUrl = url
-            isImageUploaded = true
-            Alert.alert('Image has been uploaded. You can share now')
-          }).catch((e) => console.log('Error getting download URL => ', e));
-      }).catch((e) => console.log('uploading image error => ', e));
+const downloadImage = async () => {
+  try {
+    setLoading(true); // set loading to true
+    const uri = await captureRef(viewRef, {
+      format: 'png',
+      quality: 0.8,
+    });
 
-      console.log(45)
-      Alert.alert(
-        '',
-        'Image saved successfully.',
-        [{text: 'OK', onPress: () => {}}],
-        {cancelable: false},
-      );
-    } catch (error) {
-      console.log("The error is" + error);
+    if (Platform.OS === 'android') {
+      const granted = await getPermissionAndroid();
+      if (!granted) {
+        setLoading(false); // set loading to false
+        return;
+      }
     }
-  };
+
+    console.log(uri);
+    const currentUser = auth().currentUser;
+    const uid = currentUser.uid;
+    const imgName = '1787.png';
+    const reference = storage().ref().child(uid).child(imgName);
+    const task = reference.putFile(uri);
+    console.log(loading)
+    task.then(() => {
+      console.log('Image uploaded to the bucket!');
+      setLoading(false); 
+      reference.getDownloadURL().then((url) => {
+        console.log('Download URL:', url);
+        downloadUrl = url
+        isImageUploaded = true
+      }).catch((e) => console.log('Error getting download URL => ', e));
+    }).catch((e) => console.log('uploading image error => ', e));
+
+    console.log(45)
+  } catch (error) {
+    console.log("The error is" + error);
+  } 
+};
 
   const panResponder = useRef(
     PanResponder.create({
@@ -146,74 +181,96 @@ function Editor({route, navigation}) {
 
   return (
     <View style={styles.container}>
-      <ViewShot
-        style={{height: "70%"}}
-        ref={viewRef}
-        options={{
-          fileName: 'Your-File-Name',
-          format: 'jpg',
-          quality: 0.9,
-        }}>  
-        <View style={{width: '100%', height: '100%'}}>
+    <ViewShot
+      style={{height: "70%"}}
+      ref={viewRef}
+      options={{
+        fileName: 'Your-File-Name',
+        format: 'jpg',
+        quality: 0.9,
+      }}>
+      <View style={{width: '100%', height: '100%'}}>
         <ImageBackground source={{uri: route.params.image_url}}>
           <View style={{height: "100%"}}>
-          <Animated.Text
-        style={[
-          styles.draggableText,
-          {fontSize: sliderValue},
-          {color: txtcolor},
-          {
-            transform: [{translateX: pan.x}, {translateY: pan.y}],
-          },
-        ]}
-            {...panResponder.panHandlers}>
-            {text}
-          </Animated.Text>
+            <Animated.Text
+              style={[
+                styles.draggableText,
+                {fontSize: sliderValue},
+                {color: txtcolor},
+                {
+                  transform: [{translateX: pan.x}, {translateY: pan.y}],
+                },
+              ]}
+              {...panResponder.panHandlers}>
+              {text}
+            </Animated.Text>
           </View>
         </ImageBackground>
-        </View>
-        </ViewShot>
-      <View style={{height: '30%', backgroundColor: '#000000', padding: 0, margin: 0}}>
-        {editing ? (
-          <View style={styles.inputContainer}>
-            <Text style={{color: 'white', marginTop: 12}}>Font Size</Text>
-            <View style={styles.slidercontainer}>
-              <Slider thumbTintColor="white" minimumTrackTintColor="#FFFFFF" maximumTrackTintColor="white" minimumValue={24} maximumValue={60} step={5} stepValue={1} onValueChange={(id) => setSliderValue(id)} sliderValue={sliderValue} />
-              <Slider style={styles.slider} minimumValue={0} maximumValue={10} minimumTrackTintColor="#FFFFFF" maximumTrackTintColor="#000000" thumbTintColor={txtcolor} onValueChange={handleColorChange}/>
-            </View>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter Quote here"
-              placeholderTextColor="gray"
-              onChangeText={setText}
-              onSubmitEditing={handleInputSubmit}
-              value={text}
-            />
-          </View>
-        ) : (
-          <View className="flex flex-row justify-around items-center">
-            <TouchableOpacity className="bg-[#141519] rounded-lg w-24 flex flex-col items-center p-2" onPress={handleEdit}>
-              <Image source={require('../assets/edit.png')} />
-              <Text style={{color: 'white'}}>Edit</Text>
-            </TouchableOpacity>
-            <TouchableOpacity className="bg-[#141519] rounded-lg w-24 flex flex-col items-center p-2" onPress={downloadImage}>
-              <Image source={require('../assets/save.png')} />
-              <Text style={{color: 'white'}}>Save</Text>
-            </TouchableOpacity>
-            <TouchableOpacity className="bg-[#141519] rounded-lg w-24 flex flex-col items-center p-2" onPress={shareImage}>
-              <Image source={require('../assets/share.png')} />
-              <Text style={{color: 'white'}}>Share</Text>
-            </TouchableOpacity>
-          </View>
-        )}
       </View>
+    </ViewShot>
+    {loading && (
+        <View style={styles.overlay}>
+          <ActivityIndicator size="large" color="#ffffff" />
+          <Text style={styles.overlayText}>Uploading Image...</Text>
+        </View>
+      )}
+    <View style={{height: '30%', backgroundColor: '#000000', padding: 0, margin: 0}}>
+      {editing ? (
+        <View style={styles.inputContainer}>
+          <Text style={{color: txtcolor, marginTop: 12}}>Font Size</Text>
+          <View style={styles.slidercontainer}>
+            <Slider thumbTintColor="white" minimumTrackTintColor="#FFFFFF" maximumTrackTintColor="white" minimumValue={24} maximumValue={60} step={5} stepValue={1} onValueChange={(id) => setSliderValue(id)} sliderValue={sliderValue} />
+          </View>
+          <Text style={{color: txtcolor, marginTop: 22}}>Font Color</Text>
+          <View style={styles.slidercontainer}>
+             <Slider minimumValue={0} maximumValue={360} step={2} thumbTintColor={hsvToRgb(hue, 95, 20)} minimumTrackTintColor="red" maximumTrackTintColor="blue" onValueChange={handleColorChange}/>
+          </View>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter Quote here"
+            placeholderTextColor="gray"
+            onChangeText={setText}
+            onSubmitEditing={handleInputSubmit}
+            value={text}
+          />
+        </View>
+      ) : (
+        <View className="flex flex-row justify-around items-center">
+          <TouchableOpacity className="bg-[#141519] rounded-lg w-24 flex flex-col items-center p-2" onPress={handleEdit}>
+            <Image source={require('../assets/edit.png')} />
+            <Text style={{color: 'white'}}>Edit</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-[#141519] rounded-lg w-24 flex flex-col items-center p-2" onPress={downloadImage}>
+            <Image source={require('../assets/save.png')} />
+            <Text style={{color: 'white'}}>Save</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-[#141519] rounded-lg w-24 flex flex-col items-center p-2" onPress={shareImage}>
+            <Image source={require('../assets/share.png')} />
+            <Text style={{color: 'white'}}>Share</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
+  </View>  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  overlay: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlayText: {
+    marginTop: 20,
+    color: '#fff',
+    fontSize: 18,
   },
   draggableBox: {
     backgroundColor: 'rgba(255, 255, 255, 0.7)',
@@ -238,7 +295,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     paddingHorizontal: 10,
     marginBottom: 0, // add this to reduce the gap
-    padding: 0, margin: 0
+    padding: 0, margin: 1
   },
   input: {
     fontSize: 18,
@@ -248,30 +305,9 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     borderWidth: 2,
     borderColor: 'white',
-    marginTop: 35
+    marginTop: 45
   },
-  editButtonContainer: {
-    position: 'absolute',
-    bottom: 20,
-    width: '100%',
-    paddingLeft: 320,
-    alignItems: 'center',
-  },
-  editButton: {
-    padding: 10,
-    elevation: 3,
-    opacity: 0.8,
-  },
-  editButtonText: {
-    color: 'red',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  saveButton: {
-    padding: 10,
-    elevation: 3,
-    opacity: 0.8,
-  },
+
   slidercontainer: {
     flex: 1,
     color: 'white',
@@ -285,6 +321,9 @@ const styles = StyleSheet.create({
     fontSize: 14,
     paddingVertical: 10,
   },
+  slider: {
+    borderRadius: 50,
+  }
 });
 
 export default Editor;
--- screens/Genbet.js
+++ screens/Genbet.js
@@ -3,6 +3,7 @@ import { Appbar} from 'react-native-paper';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
 import axios from "axios"
+import { ScrollView } from 'react-native-virtualized-view';
 
 import { useEffect, useState } from 'react';
 
@@ -10,6 +11,67 @@ function Genbet({navigation, route}) {
   const fs = Dimensions.get('window').fontScale;
   let req = route.params.getreq;
   let ttt = route.params.tes;
+  const prt = "From all the quotes that you know give me four quotes related to the topic of \"" + ttt + "\""
+  const [q1, setQ1] = useState("");
+  const [q2, setQ2] = useState("");
+  const [q3, setQ3] = useState("");
+  const [q4, setQ4] = useState("");
+  const [selectedQuote, setSelectedQuote] = useState(null);
+
+  const handlequote = async() => {
+    console.log("quote")
+    let data = JSON.stringify({
+      "model": "command-xlarge-beta",
+      "prompt": prt,
+      "max_tokens": 150,
+      "temperature": 0.4,
+      "k": 0,
+      "stop_sequences": [],
+      "return_likelihoods": "NONE"
+    });
+    let configure = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://api.cohere.ai/v1/generate',
+      headers: { 
+        'Authorization': 'BEARER M9ebThESCtfKc7fpDg3NI9FmKrqohwicmaziWDE1', 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };  
+    console.log(4)
+    axios.request(configure)
+    .then(response => {
+      console.log(response.data.generations);
+      response.data.generations.forEach((generation, index) => {
+        const textWithinQuotes = generation.text.match(/"([^"]*)"/)[1];
+        console.log(textWithinQuotes);
+        switch (index) {
+          case 0:
+            setQ1(textWithinQuotes);
+            break;
+          case 1:
+            setQ2(textWithinQuotes);
+            break;
+          case 2:
+            setQ3(textWithinQuotes);
+            break;
+          case 3:
+            setQ4(textWithinQuotes);
+            break;
+          default:
+            break;
+        }
+        console.log(23)
+        console.log(q1)
+        console.log(q2)
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    }); 
+  }
+  
 
   console.log('Start');
   const [img1, setImg1] = useState(
@@ -70,51 +132,78 @@ useEffect(() => {
 
 
   useEffect(() => {
-  }, [img1, img2, img3, img4]);
+  }, [img1, img2, img3, img4, q1, q2, q3, q4]);
 
-  return (
-    (img1 === '' || img2 === '' || img3 === '' || img4 === '') ? 
-    <View className="h-full">  
+  useEffect(() => {
+    try {
+      handlequote();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+
+
+return (
+  (q1 === '' || q2 === '' || q3 === '' || q4 === '' ||  img1 === '' || img2 === '' || img3 === '' || img4 === '') ? 
+  <View className="h-full">  
     <Appbar.Header style={{backgroundColor: '#1D1D1D'}}>
         <Appbar.BackAction color='white' onPress={() => {navigation.navigate('Create')}}>
         </Appbar.BackAction>
         <Appbar.Content color="white" title="Prompt Results">
         </Appbar.Content>
     </Appbar.Header>
-      <View style={styles.loading}>
-        <ActivityIndicator size="large" color="white" style={styles.spinner} />
-        <Text style={styles.loadingText}>Loading...</Text>
-      </View>
-      </View>
-      :
-      <View className="h-full">  
-          <Appbar.Header style={{backgroundColor: '#1D1D1D'}}>
-              <Appbar.BackAction color='white' onPress={() => {navigation.navigate('Create')}}>
-              </Appbar.BackAction>
-              <Appbar.Content color="white" title="Prompt Results">
-              </Appbar.Content>
-          </Appbar.Header>
-          <View className="overflow-hidden bg-stone-900 flex flex-col items-center justify-start h-full" style={{paddingLeft: 21, paddingRight: 27, paddingBottom: 34}}>
-            <Text className="text-white-0 text-left " style={{fontSize: 12 / fs, marginTop: 30}}>Showing results for "{ttt}"</Text>
-            <View className="flex flex-row items-start justify-center" style={{marginTop: 0}}>
-              <FlatList    
-                columnWrapperStyle={{justifyContent: 'space-between'}}
-                data={[
-                  { key: 'image1', uri: img1},
-                  { key: 'image2', uri: img2},
-                  { key: 'image3', uri: img3},
-                  { key: 'image4', uri: img4},
-                ]}
-                numColumns = {2}
-                renderItem={({ item }) => (
-                      <TouchableOpacity onPress={() => navigation.navigate('edit', {image_url: item.uri})} style={[styles.button]}>
-                        <Image style={styles.image} source={{ uri: item.uri }} resizeMode='cover'/>
-                      </TouchableOpacity>
-                    )}/>
-            </View> 
-          </View>
+    <View style={styles.loading}>
+      <ActivityIndicator size="large" color="white" style={styles.spinner} />
+      <Text style={styles.loadingText}>Loading...</Text>
+    </View>
+  </View>
+  :
+  <View className="h-full">  
+    <Appbar.Header style={{backgroundColor: '#1D1D1D'}}>
+      <Appbar.BackAction color='white' onPress={() => {navigation.navigate('Create')}}>
+      </Appbar.BackAction>
+      <Appbar.Content color="white" title="Prompt Results">
+      </Appbar.Content>
+    </Appbar.Header>
+    <ScrollView style={{ flex: 1 }}>
+      <View className="overflow-hidden bg-stone-900 flex flex-col items-center justify-start" style={{paddingLeft: 21, paddingRight: 27, paddingBottom: 34}}>
+        <Text className="text-white-0 text-left " style={{fontSize: 12 / fs, marginTop: 30}}>Showing results for "{ttt}"</Text>
+        <View className="overflow-hidden bg-stone-900 flex flex-col items-center justify-start" style={{paddingLeft: 21, paddingRight: 27, paddingTop: 26}}>
+          <FlatList
+            data={[q1, q2, q3, q4]}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+              onPress={() => setSelectedQuote(item)}
+              style={[styles.quote, selectedQuote === item && { backgroundColor: '#6983ce' }]}
+            >
+              <Text style={{ color: "white" }}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          />
         </View>
-  );
+        <View className="flex flex-row items-start justify-center" style={{marginTop: 8}}>
+          <FlatList    
+            columnWrapperStyle={{justifyContent: 'space-between'}}
+            data={[
+              { key: 'image1', uri: img1},
+              { key: 'image2', uri: img2},
+              { key: 'image3', uri: img3},
+              { key: 'image4', uri: img4},
+            ]}
+            numColumns = {2}
+            renderItem={({ item }) => (
+                  <TouchableOpacity onPress={() => navigation.navigate('edit', {image_url: item.uri, quote: selectedQuote})} style={[styles.button]}>
+                    <Image style={styles.image} source={{ uri: item.uri }} resizeMode='cover'/>
+                  </TouchableOpacity>
+                )}/>
+        </View> 
+      </View>
+    </ScrollView>
+  </View>
+);
+
 }
 
 const styles = StyleSheet.create({
@@ -125,8 +214,9 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   image: {
-    height: Dimensions.get("window").width*0.4,
+    height: Dimensions.get("window").width*0.6,
     width: Dimensions.get("window").width*0.4,
+    margin: 5
   },
   loading: {
     flex: 1,
@@ -141,6 +231,21 @@ const styles = StyleSheet.create({
   },
   spinner: {
     marginTop: 20
+  },
+  quote: {
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: '#ccc',
+    padding: 12,
+    margin: 5,
+    width: "100%",
+  },
+  selectedQuote: {
+    borderColor: 'blue'
+  },
+  quoteText: {
+    fontSize: 16,
+    fontWeight: 'bold'
   }
 })
 
