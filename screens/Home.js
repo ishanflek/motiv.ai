3bf1b20b575713eda5ae73311c86072d513ffe27
Mr.Dey <80757858+td-heisenbug@users.noreply.github.com>
2023-04-19 10:23:20 +0530

    Fixed main screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, Image, Dimensions, FlatList } from 'react-native';
+import { Text, View, Image, Dimensions, TouchableOpacity, FlatList } from 'react-native';
 import { Appbar, Button, TextInput} from 'react-native-paper';
 import storage from '@react-native-firebase/storage';
 import auth from '@react-native-firebase/auth';
 import { ScrollView } from 'react-native-virtualized-view';
 
 function Home({navigation}) {
-  const fs = Dimensions.get("window").fontScale;
+  const fs = Dimensions.get("window").fontScale;  
+  const { height,width } = Dimensions.get('window');
 
   const [sampleImages, setSampleImages] = useState([]);
 
@@ -23,25 +24,25 @@ function Home({navigation}) {
   }, []);
 
   return (
-    <View>
+    <View className="bg-stone-900">
       <ScrollView showVerticalScrollIndicator={false}>
-        <View className="overflow-hidden bg-stone-900 relative" style={{height: 1485, width: '100%', paddingTop: 50}}>
-          <View className="absolute flex flex-col items-start justify-between w-full" style={{width: '100%', marginTop: 70, paddingLeft: 24, paddingRight: 21}}>
+        <View className="bg-stone-900 relative" style={{width: '100%', paddingTop: 50}}>
+          <View className="flex flex-col items-start justify-between w-full" style={{width: '100%', marginTop: 70}}>
               <View style={{flexDirection: 'row'}}>
               <Image style={{marginRight: 15, marginTop: 20}} source={require("../assets/247_1993.png")} alt=""></Image>
               <Text className="text-neutral-201 text-left" style={{ color: 'white', marginTop: 30, marginBottom: 13, fontSize: 33 / fs}}>Motiv.ai</Text>
               </View>
-            <TextInput className="w-full" style={{marginLeft: 3, marginTop: 19}} label="Describe your thoughts" mode="flat"></TextInput>
             <View className="flex flex-row items-center justify-between w-full">
-              <Text className="text-white text-left" style={{marginRight: 220, marginTop: 40, marginBottom: 2, fontSize: 14 / fs, color: 'white'}}>Recents</Text>
+              <Text className="text-white text-left" style={{padding: 15, marginRight: 220, marginTop: 40, marginBottom: 2, fontSize: 14 / fs, color: 'white'}}>Recents</Text>
               <Image style={{marginTop: 30}}source={require("../assets/238_6397.png")} alt=""></Image>
             </View>
             <FlatList
-            columnWrapperStyle={{justifyContent: 'space-between'}}
+          style={{marginLeft: width*0.02}}
             data={sampleImages}
             numColumns={2}
             renderItem={({item}) => (
-              <View style={{padding: 10, width: "48%", height: 280}}>
+              <View style={{padding: width*0.02, width: "49%", height: height/3.5}}>
+                <TouchableOpacity onPress={() => navigation.navigate('prew', {imgurl: item})}>
                 <Image
                   style={{
                     width: '100%',
@@ -51,7 +52,8 @@ function Home({navigation}) {
                   source={{uri: item}}
                   resizeMode="cover"
                 />
-                </View>
+                </TouchableOpacity>
+              </View>
             )}
           />
           </View>
